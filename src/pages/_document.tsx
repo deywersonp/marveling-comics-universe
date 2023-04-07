7442ce3f93c40e71e39bfd8445512aa31133bc1d
Deywerson Pereira <deywerson.pereira@gmail.com>
2023-04-06 21:45:12 -0300

    refactor: removed unused content and added new favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt">
+      <Head>
+        <meta name="description" content="Adquira suas comics favoritas do universo Marvel" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
