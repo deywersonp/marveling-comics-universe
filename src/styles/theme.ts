a5954ee40bf88eb6b82b524c0ab7800ee6490d80
Deywerson Pereira <deywerson.pereira@gmail.com>
2023-04-07 18:24:55 -0300

    chore: added chakra ui config

---
+++ src/styles/theme.ts
@@ -0,0 +1,37 @@
+import { Lato } from 'next/font/google';
+import { extendTheme } from '@chakra-ui/react';
+
+const lato = Lato({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+})
+
+export const theme = extendTheme({
+  colors: {
+    gray: {
+      "900": "#151515",
+      "800": "#1F2029",
+      "700": "#353646",
+      "600": "#4B4D63",
+      "500": "#616480",
+      "400": "#797D9A",
+      "300": "#9699B0",
+      "200": "#B3B5C6",
+      "100": "#D1D2DC",
+      "50": "#F7FAFC",
+    },
+  },
+  fonts: {
+    heading: lato.style.fontFamily,
+    body: lato.style.fontFamily,
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'gray.900',
+        color: 'gray.50'
+      }
+    }
+  }
+})
