7bebef9f8fea11e5f0265de741c6ff6a317df644
Deywerson Pereira <deywerson.pereira@gmail.com>
2023-04-10 18:18:19 -0300

    feat: add bag icon at header component

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,6 +1,9 @@
-import { Flex } from '@chakra-ui/react';
-import { Logo } from '@/components/Logo';
 import Link from 'next/link';
+import { Flex, IconButton } from '@chakra-ui/react';
+
+import { Logo } from '@/components/Logo';
+
+import { HiShoppingBag } from 'react-icons/hi';
 
 export const Header = () => {
   return (
@@ -8,16 +11,29 @@ export const Header = () => {
       as="header"
       w="100%"
       maxWidth={1440}
-      h="20"
+      height={["40", "20"]}
       mx="auto"
       mt="4"
       px="8"
       align="center"
+      flexDir={["column", "row"]}
       justifyContent={["center", "flex-start"]}
     >
       <Link href="/">
         <Logo />
       </Link>
+
+      <IconButton
+        as={Link}
+        href="/cart"
+        aria-label="Go to cart"
+        icon={<HiShoppingBag size={30} />}
+        variant="ghost"
+        colorScheme='red'
+        borderRadius="full"
+        ml="auto"
+        mt={["5", "0"]}
+      />
     </Flex>
   )
 };
